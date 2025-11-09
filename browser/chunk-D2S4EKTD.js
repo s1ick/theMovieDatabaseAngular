dc6def6ca0ef10de1aecde183adfba429886ea0c
Ivan Matveev <berkut89@list.ru>
2025-11-09 12:40:57 +1200

    Auto-generated commit

---
+++ browser/chunk-D2S4EKTD.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-OU52GKYE.js";import"./chunk-Z46XBT6O.js";export{a as MovieDetailsComponent};
---
+++ browser/chunk-RRETGLKD.js
@@ -0,0 +1,1 @@
+import{n as a}from"./chunk-OXCO646N.js";import"./chunk-Z46XBT6O.js";export{a as HomeComponent};
